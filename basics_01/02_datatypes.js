79a549f58406f5eab85257876a7d532a90942a3b
shrishtigohel <shrish2309@gmail.com>
2025-06-26 12:12:12 +0000

    02_datatypes

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,17 @@
+//alert (" 3 + 3 ");  //we are using notejs, not browser
+
+
+let name = "Shrish";
+let age = 14;
+let isLoggedIn = true;
+
+
+// number => 123;
+// BigInt
+// string => ""
+// boolean => true/false;
+// null => no value/standalone value
+// undefined =>
+// symbol => unique value
+
+console.log(typeof undefined);
