d2c7e1560e06056480751b8dd56f24432ff96e3e
shrishtigohel <shrish2309@gmail.com>
2025-06-26 13:19:19 +0000

    03_conversionOperation.

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = null;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0;
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "Shrish" => true;
+
+let someNumber = 33;
+
+let someString = String(someNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
