62d440b33978b1834a8d5cc53c82f4554e973ef7
shrishtigohel <shrish2309@gmail.com>
2025-06-26 09:11:57 +0000

    01_variables

---
+++ basics_01/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123;
+let accountMail = 'shrish2309@google.com';
+var accountpassword = "56789";
+accountCity = "Ahmedabad";
+let accountState;
+
+// accountId = 456; 
+console.log(accountId);
+
+
+accountMail = 'sunny@google.com';
+accountpassword = "101010";
+accountCity = "Pune";
+
+console.table([accountId, accountMail, accountpassword, accountCity,accountState]);
