29f076d89ba274104ac158cb2744f64629494784
shrishtigohel <shrish2309@gmail.com>
2025-07-01 12:30:57 +0000

    05_datatypes_summary

---
+++ basics_01/05_datatypes_summary.js
@@ -0,0 +1,29 @@
+// Primitive Datatypes
+
+// 7 types: String, Number, BigInt, Boolean, Undefined, Null, Symbol
+
+
+
+const score = 100; // Number
+const name = "Shrish"; // String
+const isPassed = true; // boolean
+const bigNumber = 123456789n; // BigInt
+const notDefined = undefined //undefined
+const emptyValue = null; //null
+const uniqueId = Symbol("id"); // Symbol
+
+
+// Non-Primitive Datatypes (Refernce Types)
+
+// 3 types: Object, Array, Function 
+
+const person = {
+    name: "Shrishti",
+    age: 25,
+} // Object
+
+const numbers = [1,2,3,4,5]; // Array
+
+function MyFunction() {
+    console.log("hello world");
+} // Function
