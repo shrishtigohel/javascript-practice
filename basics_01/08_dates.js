3a9c6870fb8464613f8d45555fe56161ce6e94d0
shrishtigohel <shrish2309@gmail.com>
2025-07-07 07:11:14 +0000

    07 and 08

---
+++ basics_01/08_dates.js
@@ -0,0 +1,20 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+
+
+let CreateDate = new Date(2023, 8, 23);
+console.log(CreateDate.toString());
+
+
+let timesStamp = Date.now();
+console.log(Math.floor(timesStamp / 1000)); // seconds
+console.log(Math.floor(timesStamp / 60)); // Minutes
+console.log(Math.floor(timesStamp / 3600)); // Hours
+console.log(Math.floor(timesStamp / 3600 * 24)); //days
+console.log(Math.floor(timesStamp / 3600 * 24 * 30)); // Months
+console.log(Math.floor(timesStamp / 3600 * 24 * 365));  // Years
+console.log(timesStamp);
+
+
